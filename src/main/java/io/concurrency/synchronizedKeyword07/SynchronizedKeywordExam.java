41cbf91d0452b72cb68bed0ad0fe4952006a17c3
mason park <joung4342@gmail.com>
2024-04-01 20:06:03 +0900

    자바 synchronized 키워드 예제2(서로다른 모니터)

--- src/main/java/io/concurrency/synchronizedKeyword07/SynchronizedKeywordExam.java
+++ src/main/java/io/concurrency/synchronizedKeyword07/SynchronizedKeywordExam.java
@@ -8,25 +8,28 @@ public class SynchronizedKeywordExam {
 
     public synchronized  void instanceMethod() {
         instanceCount++;
-        System.out.println("instance 메소드 동기화"+ instanceCount);
+        staticCount++;
+        System.out.println("instance 메소드 동기화 "+ instanceCount);
+
     }
 
     public synchronized static void staticMethod() {
         staticCount++;
-        System.out.println("static 메소드 동기화"+ staticCount);
+        System.out.println("static 메소드 동기화 "+ staticCount);
     }
 
     public void instanceBlock() {
         synchronized (this) {
             instanceCount++;
-            System.out.println("instance 메소드 동기화"+ instanceCount);
+            staticCount++;
+            System.out.println("instance 메소드 동기화 "+ instanceCount);
         }
     }
 
     public static void staticBlock() {
         synchronized (SynchronizedKeywordExam.class) {
             staticCount++;
-            System.out.println("static 메소드 동기화"+ staticCount);
+            System.out.println("static 메소드 동기화 "+ staticCount);
         }
     }
 
