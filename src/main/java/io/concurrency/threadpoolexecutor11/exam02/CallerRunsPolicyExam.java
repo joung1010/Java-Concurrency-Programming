c46d8c2d2606b8ae00197ae2cbe065364227bf55
mason park <joung4342@gmail.com>
2024-08-21 20:46:34 +0900

    feat(ThreadPoolExecutor): RejectedExecutionHandler 예제

---
+++ src/main/java/io/concurrency/threadpoolexecutor11/exam02/CallerRunsPolicyExam.java
@@ -0,0 +1,46 @@
+package io.concurrency.threadpoolexecutor11.exam02;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * <b> CallerRunsPolicyExam </b>
+ *
+ * @author jh.park
+ * @version 0.1.0
+ * @since 2024-08-21
+ */
+public class CallerRunsPolicyExam {
+    public static void main(String[] args) {
+        int corePoolSize = 2;
+        int maximumPoolSize = 2;
+        long keepAliveTime = 0L;
+        int workQueueCapacity = 2;
+
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(corePoolSize
+                , maximumPoolSize
+                , keepAliveTime
+                , TimeUnit.SECONDS
+                , new ArrayBlockingQueue<Runnable>(workQueueCapacity)
+                , new ThreadPoolExecutor.AbortPolicy());
+
+
+        submitTask(threadPoolExecutor);
+    }
+
+    private static void submitTask(ThreadPoolExecutor executor) {
+        for (int i = 0; i < 5; i++) {
+            final int taskId = i;
+            executor.execute(() -> {
+                System.out.println("taskId: " + taskId + "is Running on thread: " + Thread.currentThread().getName());
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        }
+        executor.shutdown();
+    }
+}
